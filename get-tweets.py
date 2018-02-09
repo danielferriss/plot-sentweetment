3d626d866716aa950f4166722fcb3ff3d52e2e07
Daniel <danielrferriss@gmail.com>
2018-02-08 23:32:08 -0600

    senses language and only uses english tweets

--- get-tweets.py
+++ get-tweets.py
@@ -1,14 +1,13 @@
 import tweepy
 import numpy as np
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+from langdetect import detect
 
 #Secret Twitter Keys
 consumer_key = 'secret'
 consumer_secret = 'secret'
 access_token = 'secret'
 access_token_secret = 'secret'
-
-
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -17,17 +16,25 @@ api = tweepy.API(auth)
 #sentiment analysis
 sid = SIA()
 
+#enter search word in console
+search_term = input("Enter what word you would like to search for: ")
+
 # override tweepy.StreamListener to add logic to on_status
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
         tweet = status.text
-        author = status.author.screen_name
-        sentiment = sid.polarity_scores(tweet)
-
-        print(author, '\n', tweet, '\n', sentiment, '\n\n')
+        #langdetect throws an error if the tweet is only a link or only emojis, or a mix of the two.
+        #This stops that from happening
+        try:
+            if detect(tweet) == 'en':
+                author = status.author.screen_name
+                sentiment = sid.polarity_scores(tweet)
+                print(author, '\n', tweet, '\n', sentiment, '\n\n')
+        except:
+        	pass
 
 myStreamListener = MyStreamListener()
 myStream = tweepy.Stream(auth = api.auth, listener=MyStreamListener())
 
-myStream.filter(track=['microsoft'])
+myStream.filter(track=[search_term])
